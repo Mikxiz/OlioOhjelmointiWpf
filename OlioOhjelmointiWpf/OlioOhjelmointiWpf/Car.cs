92cfdb48d668936c1cb82e28b0e4982fec861a17
Mikxiz <38124633+Mikxiz@users.noreply.github.com>
2018-10-10 10:51:01 +0300

    Add files via upload

---
+++ OlioOhjelmointiWpf/OlioOhjelmointiWpf/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlioOhjelmointiWpf
+{
+    public class Car
+    {
+        // kaksi ominaisuutta (property)
+        public string Color { get; set; }
+        public string Model { get; set; }
+
+        // kenttä (field)
+        public string DoorCount;
+
+        private int maxSpeed;
+
+        public int MaxSpeed {
+            get
+            {
+                return maxSpeed;
+            }
+        set
+            {
+                if ((value > 0) && (value < 500))
+                {
+                    maxSpeed = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public void StartEngine()
+        {
+        }
+        public void StopEngine()
+        {
+        }
+        
+    }
+}
---
+++ OlioOhjelmointiWpf/OlioOhjelmointiWpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OlioOhjelmointiWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Car myCar = new Car();
+            Car Chevy = new Car();
+
+            int maxSpeed = int.Parse(maxSpeedTextBox.Text);
+            myCar.Color = "Grey";
+            myCar.MaxSpeed = maxSpeed;
+            MessageBox.Show("Huippunopeus asetettu ominaisuuden kautta.");
+            myCar.Model = "Corolla";
+            myCar.StartEngine();
+            ShowCarInfo(myCar);
+
+            Chevy.Color = "Red";
+            Chevy.MaxSpeed = 300;
+            Chevy.Model = "Corvette";
+            Chevy.StartEngine();
+            ShowCarInfo(Chevy);
+
+            MessageBox.Show(Chevy.ToString());
+        }
+            public void ShowCarInfo(Car c)
+            {
+                string message = "Model: " + c.Model + "\r\n" +
+                    "Color: " + c.Color + "\r\n" +
+                    "Max speed: " + c.MaxSpeed;
+                MessageBox.Show(message);
+            }
+    }
+}
